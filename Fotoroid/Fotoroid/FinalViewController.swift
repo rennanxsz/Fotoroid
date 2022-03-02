b9e6e0f5710d5ebca14ca5b7fa305e2469d6d367
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-01 22:30:31 -0300

    Creating constraints and cocoa touch class
    Criando as contraints necessárias e as classes a serem utilizadas.

---
+++ Fotoroid/Fotoroid/FinalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinalViewController.swift
+//  Fotoroid
+//
+//  Created by Rennan Bruno on 01/03/22.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
