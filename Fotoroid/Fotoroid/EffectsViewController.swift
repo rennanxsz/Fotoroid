86377f265521c74537001a4517c0f2f500810bab
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-01 22:45:24 -0300

    Final layout
    Terminado o layout a ser utilizado junto com as outlets e actions a serem utilizadas.

--- Fotoroid/Fotoroid/EffectsViewController.swift
+++ Fotoroid/Fotoroid/EffectsViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class EffectsViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var viLoading: UIView!
+    @IBOutlet weak var ivPhoto: UIImageView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Fotoroid/Fotoroid/FinalViewController.swift
+++ Fotoroid/Fotoroid/FinalViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class FinalViewController: UIViewController {
 
+    @IBOutlet weak var ivPhoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func save(_ sender: UIButton) {
     }
-    */
-
+    @IBAction func restart(_ sender: UIButton) {
+    }
+    
+    
 }
