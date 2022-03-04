19d0932495440715f1f3a6159b5020d26a1ea2a9
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-03 22:50:06 -0300

    bugfix
    Corrigido o bug das imagens e filtros.

--- Fotoroid/Fotoroid/EffectsViewController.swift
+++ Fotoroid/Fotoroid/EffectsViewController.swift
@@ -14,13 +14,24 @@ class EffectsViewController: UIViewController {
     @IBOutlet weak var ivPhoto: UIImageView!
     
     var image: UIImage!
+    lazy var filterManager: FilterManager = {
+       let filterManager = FilterManager(image: image)
+        return filterManager
+        
+    }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         ivPhoto.image = image
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        ivPhoto.image = filterManager.applyFilter(type: FilterType(rawValue: 0)!)
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
