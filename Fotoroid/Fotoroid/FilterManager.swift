01df9dfc56f722cc425fdb2c00944d117fbbcd87
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-03 22:02:53 -0300

    Configuration image filter
    Configurando os filtros a serem utilizados na imagem

--- Fotoroid/Fotoroid/FilterManager.swift
+++ Fotoroid/Fotoroid/FilterManager.swift
@@ -36,7 +36,28 @@ class FilterManager {
     }
     
     func applyFilter(type: FilterType) -> UIImage {
+        let ciImage = CIImage(image: originalImage)!
+        let filter = CIFilter(name: filterNames[type.hashValue])!
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
         
+        switch type {
+            case .comic:
+                break
+            case .sepia:
+                filter.setValue(1.0, forKey: kCIInputIntensityKey)
+            case .halftone:
+                filter.setValue(25, forKey: kCIInputWidthKey)
+            case .crystallize:
+                filter.setValue(25, forKey: kCIInputRadiusKey)
+            case .vignette:
+                filter.setValue(3, forKey: kCIInputIntensityKey)
+                filter.setValue(30, forKey: kCIInputRadiusKey)
+            case .noir:
+                break
+        }
+        let filteredImage = filter.value(forKey: kCIOutputImageKey) as! CIImage
+        let cgImage = context.createCGImage(filteredImage, from: filteredImage.extent)
+        return UIImage(cgImage: cgImage!)
     }
     
 }
