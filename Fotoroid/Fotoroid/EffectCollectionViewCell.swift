5a51967edfd29a5b825f397693ae49464ab20596
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-03-04 22:30:06 -0300

    using and configuring the scroll
    Usando o scrow view e também configurando

---
+++ Fotoroid/Fotoroid/EffectCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EffectCollectionViewCell.swift
+//  Fotoroid
+//
+//  Created by Rennan Bruno on 04/03/22.
+//
+
+import UIKit
+
+class EffectCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ivEffect: UIImageView!
+}
--- Fotoroid/Fotoroid/EffectsViewController.swift
+++ Fotoroid/Fotoroid/EffectsViewController.swift
@@ -20,23 +20,57 @@ class EffectsViewController: UIViewController {
         
     }()
     
+    let filterImageNames = [
+        "comic",
+        "sepia",
+        "halftone",
+        "crystallize",
+        "vignette",
+        "noir"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         ivPhoto.image = image
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        ivPhoto.image = filterManager.applyFilter(type: FilterType(rawValue: 0)!)
-    }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
+    
+    func showLoading(_ show: Bool) {
+        viLoading.isHidden = !show
+    }
 
 
+}
+
+extension EffectsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filterManager.filterNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EffectCollectionViewCell
+        
+        cell.ivEffect.image = UIImage(named: filterImageNames[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let type = FilterType(rawValue: indexPath.row) {
+            showLoading(true)
+            let filteredImage = self.filterManager.applyFilter(type: type)
+            self.ivPhoto.image = filteredImage
+            showLoading(false)        }
+    }
 }
